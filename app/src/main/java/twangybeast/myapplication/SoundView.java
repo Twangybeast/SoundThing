8c7fa6393290ca440db89aacb79cfa4b167d5aa3
Cheng <ChenDan19@issaquah.wednet.edu>
2018-02-12 08:01:34 -0800

    Initial Commit

---
+++ app/src/main/java/twangybeast/myapplication/SoundView.java
@@ -0,0 +1,63 @@
+package twangybeast.myapplication;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class SoundView extends SurfaceView implements SurfaceHolder.Callback{
+
+    public SoundView(Context context) {
+        super(context);
+    }
+
+    public SoundView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SoundView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+        // TODO: consider storing these as member variables to reduce
+        // allocations per draw cycle.
+        int paddingLeft = getPaddingLeft();
+        int paddingTop = getPaddingTop();
+        int paddingRight = getPaddingRight();
+        int paddingBottom = getPaddingBottom();
+
+        int contentWidth = getWidth() - paddingLeft - paddingRight;
+        int contentHeight = getHeight() - paddingTop - paddingBottom;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
